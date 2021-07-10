f9bfac8ced2ec2d67c5407786161142770eafe34
ParikhSanyam <sanyamparikh435@gmail.com>
2021-07-10 14:55:02 +0530

    deploying first github page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import profileImage from './img/yellow.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={ profileImage } alt="image" />
+      <h1>Hello Sanyam</h1>
+      <h6>Welcome to the computer world</h6>
+      <p>Thanks for visiting it is under developing please wait some time.</p>
     </div>
   );
 }
